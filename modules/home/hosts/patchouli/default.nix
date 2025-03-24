a85317df0a85f15d7918b52caf6a74b9df42c948
gabman15 <gabrielmcninch@gmail.com>
2025-03-24 15:53:36 -0400

    add wsl patchouli host

---
+++ modules/home/hosts/patchouli/default.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }:
+
+{
+  custom.home.programs.bash.enable = true;
+  
+  home.stateVersion = "24.11";
+}
--- modules/nixos/hosts/common/default.nix
+++ modules/nixos/hosts/common/default.nix
@@ -5,32 +5,13 @@
     [
       ../../hardware
       ../../programs
+      ../../suites
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
-    };
-  };
-
-  networking.networkmanager.enable = true;
-
   # Enable the OpenSSH daemon.
   custom.nixos.programs.ssh.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
 
   environment.systemPackages = with pkgs; [
     emacs
