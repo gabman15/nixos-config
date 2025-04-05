f90ccfd51ffc7b54b3594ee0c6689f8cc785bda4
gabman15 <gabrielmcninch@gmail.com>
2025-04-04 20:49:47 -0400

    move gpg-agent to home manager and make waybar a systemd service

---
+++ modules/home/programs/gnupg/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, config, ... }:
+
+with lib; let
+  cfg = config.custom.home.programs.gnupg;
+in
+  {
+    options.custom.home.programs.gnupg = {
+      enable = mkEnableOption "gnupg secret manager";
+    };
+
+    
+    config = mkIf cfg.enable {
+      services.gpg-agent = {
+        enable = true;
+        pinentryPackage = with pkgs; pinentry-gtk2;
+        enableSshSupport = true;
+        defaultCacheTtl = 86400;
+        defaultCacheTtlSsh = 86400;
+      };
+    };
+  }
