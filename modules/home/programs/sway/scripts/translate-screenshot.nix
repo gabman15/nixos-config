dbab8185f0d8d20489d183d04c1aac2fb23a04c8
gabman15 <gabrielmcninch@gmail.com>
2025-07-07 18:01:23 -0400

    kill manga ocr earlier in translate script

--- modules/home/programs/sway/scripts/translate-screenshot.nix
+++ modules/home/programs/sway/scripts/translate-screenshot.nix
@@ -11,9 +11,9 @@ pkgs.writeShellScript "translate-sway-screenshot" ''
     sleep 1
     clip=$(${pkgs.wl-clipboard}/bin/wl-paste -l)
   done
-  ${pkgs.dunst}/bin/dunstify manga-ocr "$(${pkgs.wl-clipboard}/bin/wl-paste)"
-  ${translate}
   if [ -z "$running" ]; then
     ${pkgs.util-linux}/bin/kill $PID
   fi
+  ${pkgs.dunst}/bin/dunstify manga-ocr "$(${pkgs.wl-clipboard}/bin/wl-paste)"
+  ${translate}
 ''
