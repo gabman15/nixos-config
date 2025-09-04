d420b7a2587efcb96f69e4e66e0e6501997b897f
gabman15 <gabrielmcninch@gmail.com>
2025-09-04 09:36:39 -0400

    ssh disable password auth

--- modules/nixos/programs/ssh/default.nix
+++ modules/nixos/programs/ssh/default.nix
@@ -9,6 +9,11 @@ in
     };
     
     config = mkIf cfg.enable {
-      services.openssh.enable = true;
+      services.openssh = {
+        enable = true;
+        settings = {
+          PasswordAuthentication = false;
+        };
+      };
     };
   }
