b2c091c3ba47c141ea6f3342fab57c313665966a
gabman15 <gabrielmcninch@gmail.com>
2025-09-11 23:09:27 -0400

    fix waybar cpu temp for koishi and some extra koishi configs

--- modules/nixos/hosts/koishi/default.nix
+++ modules/nixos/hosts/koishi/default.nix
@@ -19,6 +19,16 @@
     tailscale
   ];
 
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
+  #       user = "greeter";
+  #     };
+  #   };
+  # };
+
   services.greetd = {
     enable = true;
     settings = {
