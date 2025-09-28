8d9587c49f11a746e172aca17bf2ba1c9bd91e10
gabman15 <32818424+gabman15@users.noreply.github.com>
2025-09-28 13:13:07 -0400

    use monitor names for waybar bar outputs

--- modules/home/programs/waybar/hosts/koishi/bars.nix
+++ modules/home/programs/waybar/hosts/koishi/bars.nix
@@ -3,7 +3,7 @@
     layer = "top";
     position = "top";
     height = 40;
-    output = "DP-4";
+    output = "Acer Technologies XV272U 0x1121BA45";
     modules-left = [ "pulseaudio" "memory" "cpu" "temperature" ];
     modules-center = [ "clock" "custom/weather" ];
     modules-right = [ "disk" "disk#games" "disk#anime" ]; # + weather + package update?
@@ -12,7 +12,7 @@
     layer = "top";
     position = "top";
     height = 40;
-    output = "DVI-D-1";
+    output = "Dell Inc. DELL E2414H VJH96522A4TU";
     modules-left = [ "custom/mpd-button" "mpd" ];
     modules-right = [ "clock" ];
   };
