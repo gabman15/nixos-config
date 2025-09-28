80279099fb7281f0de0117c7fbabd771fceb5eb3
gabman15 <32818424+gabman15@users.noreply.github.com>
2025-09-28 13:13:07 -0400

    use tailscale for dns on patchouli

--- modules/nixos/hosts/patchouli/default.nix
+++ modules/nixos/hosts/patchouli/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [
@@ -14,35 +14,36 @@
     linger = true;
   };
 
+  environment.systemPackages = with pkgs; [
+    tailscale
+  ];
+
+  services.tailscale.enable = true;
+
   wsl = {
     enable = true;
     defaultUser = "lord_gabem";
     wslConf = {
       network.generateHosts = false;
-      network.generateResolvConf = false;
       interop.appendWindowsPath = false;
     };
   };
 
-  systemd.services.resolvconf-wsl = {
-    enable = true;
-    description = "Generate resolvconf with nameserver and search domain from windows host";
-    wantedBy = ["network-online.target"];
-    after = [ "mnt-c.mount" ];
-    script = ''
-        touch /etc/resolv.conf
+  # systemd.services.resolvconf-wsl = {
+  #   enable = true;
+  #   description = "Generate resolvconf with nameserver and search domain from windows host";
+  #   wantedBy = ["network-online.target"];
+  #   after = [ "mnt-c.mount" ];
+  #   script = ''
+  #       touch /etc/resolv.conf
 
-        # Run the PowerShell command to generate "nameserver" lines and append to /etc/resolv.conf
-        /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -Command '(Get-DnsClientServerAddress -AddressFamily IPv4).ServerAddresses | ForEach-Object { "nameserver $_" }' | tr -d '\r'| tee /etc/resolv.conf > /dev/null
+  #       # Run the PowerShell command to generate "nameserver" lines and append to /etc/resolv.conf
+  #       /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -Command '(Get-DnsClientServerAddress -AddressFamily IPv4).ServerAddresses | ForEach-Object { "nameserver $_" }' | tr -d '\r'| tee /etc/resolv.conf > /dev/null
     
-        # Run the PowerShell command to generate "search" line and append to /etc/resolv.conf
-        /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -Command '(Get-DnsClientGlobalSetting).SuffixSearchList | ForEach-Object { "search $_" }' | tr -d '\r'| tee -a /etc/resolv.conf > /dev/null
-      '';
-  };
-
-  networking.resolvconf = {
-    enable = false;
-  };
+  #       # Run the PowerShell command to generate "search" line and append to /etc/resolv.conf
+  #       /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -Command '(Get-DnsClientGlobalSetting).SuffixSearchList | ForEach-Object { "search $_" }' | tr -d '\r'| tee -a /etc/resolv.conf > /dev/null
+  #     '';
+  # };
 
   system.stateVersion = "24.11";
 }
