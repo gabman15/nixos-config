548d7724cc4e61a86e089e67e78ed5b998188be9
gabman15 <32818424+gabman15@users.noreply.github.com>
2025-09-28 13:13:07 -0400

    change gensokyo ssh key

--- modules/home/hosts/gensokyo/default.nix
+++ modules/home/hosts/gensokyo/default.nix
@@ -1,8 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   # home.packages = with pkgs; [
-    
+
   # ];
+
+  custom.home.behavior.xdg.enable = lib.mkForce false;
+
   home.stateVersion = "24.11";
 }
