fec4455086ad69ff5073de315a9f736953c343a9
gabman15 <32818424+gabman15@users.noreply.github.com>
2025-09-28 13:13:07 -0400

    suwako nvidia prime test

--- modules/nixos/hosts/suwako/default.nix
+++ modules/nixos/hosts/suwako/default.nix
@@ -73,5 +73,12 @@
     ];
   };
 
+  hardware.nvidia.prime = {
+    reverseSync.enable = true;
+
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+
   system.stateVersion = "24.11";
 }
