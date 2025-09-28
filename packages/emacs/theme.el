8e034562a24ddedfce8a2f49f50413441085ddf3
gabman15 <32818424+gabman15@users.noreply.github.com>
2025-09-28 13:13:07 -0400

    use gruvbox-dark-medium emacs theme

--- packages/emacs/theme.el
+++ packages/emacs/theme.el
@@ -3,4 +3,4 @@
 
 (add-to-list 'custom-theme-load-path (concat nix-emacs-directory "/themes"))
 (setq custom-safe-themes t) ; Treat all themes as safe
-(load-theme 'gruvbox-dark-hard)
+(load-theme 'gruvbox-dark-medium)
