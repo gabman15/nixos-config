71b7be422f2a0b838186ddc20cc31b5df3b36c8a
gabman15 <32818424+gabman15@users.noreply.github.com>
2025-09-28 13:13:07 -0400

    add translate screenshot script to sway

--- modules/home/programs/sway/scripts/screenshot.nix
+++ modules/home/programs/sway/scripts/screenshot.nix
@@ -1,14 +1,32 @@
 pkgs: config:
 
 pkgs.writeShellScript "sway-screenshot" ''
+  translation=false
+
+  while getopts ":t" opt; do
+    case $opt in
+      t)
+        translation=true
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+    esac
+  done
   home=/home/${config.home.username}
-  scrdir=$home/pictures/screenshots/grim
+  scrdir=$XDG_PICTURES_DIR/screenshots/grim
 
   ${pkgs.wayfreeze}/bin/wayfreeze --hide-cursor & PID=$!
   ${pkgs.coreutils}/bin/sleep 0.02
   area="$(${pkgs.slurp}/bin/slurp -o -w 0)"
-
-  outputfile="$(${pkgs.coreutils}/bin/date +$scrdir/%s.png)"
+  if [ $translation = true ]; then
+    mkdir -p /tmp/translation
+    outputfile="/tmp/translation/img.png"
+  else
+    mkdir -p $srcdir
+    outputfile="$(${pkgs.coreutils}/bin/date +$scrdir/%s.png)"
+  fi
 
   ${pkgs.grim}/bin/grim -g "$area" - | \
       tee $outputfile | ${pkgs.wl-clipboard}/bin/wl-copy -t image/png
