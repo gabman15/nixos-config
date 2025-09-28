ff3df4ed3df174c75b1ab54d3cfbd142736ef5de
gabman15 <32818424+gabman15@users.noreply.github.com>
2025-09-28 13:13:07 -0400

    sway and waybar for gensokyo

--- modules/home/hosts/gensokyo/default.nix
+++ modules/home/hosts/gensokyo/default.nix
@@ -27,6 +27,11 @@
   };
   custom.home.programs.rofi.enable = true;
   custom.home.programs.backgrounder.enable = true;
+  custom.home.programs.sway.enable = true;
+  wayland.windowManager.sway.package = null;
+  wayland.windowManager.sway.config.terminal = lib.mkForce "kitty";
+  custom.home.programs.waybar.enable = true;
+  custom.home.opts.stylix = true;
 
   stylix = lib.recursiveUpdate ((import ../../../nixos/themes/common) pkgs) ((import ../../../nixos/themes/${config.custom.home.opts.hostname}) pkgs);
 
